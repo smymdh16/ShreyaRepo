48d966f9762ef5a72625ac7efce8b72ba1bc4f59
Dell <Dell@DESKTOP-9KRK1A9>
2025-04-01 10:30:43 +0530

    sharing file

--- Framework15Automation/src/test/java/ContactTests/ToCreateContact.java
+++ Framework15Automation/src/test/java/ContactTests/ToCreateContact.java
@@ -4,6 +4,8 @@ public class ToCreateContact {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test script wriiten for contact successfully by kavita");
+		
 		
 		
 		
--- Framework15Automation/src/test/java/OrgTests/ToCreateOrg.java
+++ Framework15Automation/src/test/java/OrgTests/ToCreateOrg.java
@@ -4,6 +4,7 @@ public class ToCreateOrg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test script written for organization successfully by kavita");
 
 	}
 
